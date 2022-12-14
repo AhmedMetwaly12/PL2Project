ad9fa8f25e22353ff48c78bea644ac62d5d06ef8
157002a <96450287+157002a@users.noreply.github.com>
2022-12-14 18:07:56 +0200

    Add files via upload

---
+++ Exam.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Exam {
+private int no_of_questions;
+private int ans;
+private String[][] questions;
+private final int correct_ans = 1 ;
+private int sum_of_correct;
+
+
+
+
+
+
+}
---
+++ user.java
@@ -0,0 +1,14 @@
+public abstract class user {
+String username;
+String password;
+private int ID;
+
+    public user(String username, String password, int ID) {
+        this.username = username;
+        this.password = password;
+        this.ID = ID;
+    }
+
+    
+
+}
